4ef44e3c115ee75c7df6a1d8cad046f98fc352f4
Hellothereobi123 <tharun.dilliraj@gmail.com>
2026-02-22 22:37:31 -0500

    finished downconversion

--- text_transmission_decode.py
+++ text_transmission_decode.py
@@ -1,3 +1,4 @@
+import math
 def file_read(file_path): #reads the file of inputs and returns the values as a list of amplitudes
     content = []
     with open(file_path, 'r') as file:
@@ -7,11 +8,23 @@ def file_read(file_path): #reads the file of inputs and returns the values as a 
         content[i] = float(content[i])
     #print(content)
     return content
-def downconversion(amp_list):
+def downconversion(amp_list, carrier_frequency, sampling_frequency):
     sinlist = [0]*len(amp_list)
-    for i in range(len(sinlist))
-    cosval = 0
-    return sinval, cosval
+    coslist = [0]*len(amp_list)
+    for i in range(len(sinlist)):
+        sinlist[i] = amp_list[i]*math.sin(2*math.pi*carrier_frequency*i*(1/sampling_frequency))
+    for i in range(len(sinlist)):
+        coslist[i] = amp_list[i]*math.cos(2*math.pi*carrier_frequency*i*(1/sampling_frequency))
+    sinlist, coslist
+
+                   
+    return sinlist, coslist
 filepath = "./input.txt"
+carrier_freq = 20 #carrier frequency in hertz
+sampling_frequency = 100 #sampling frequency in hertz
+
 amp_list = file_read(filepath) #get the list of amplitudes from the file
+sinlist, coslist = downconversion(amp_list, carrier_freq, sampling_frequency) #gets the downconverted sin and cos lists (useful for demodulation)
+
+
 
