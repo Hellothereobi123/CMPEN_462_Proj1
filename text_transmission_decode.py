95ef206a7580b7688b23072c9d3a15dcf399926b
Hellothereobi123 <tharun.dilliraj@gmail.com>
2026-02-23 14:31:20 -0500

    changed data structure to numpy for easier matrix ops

--- text_transmission_decode.py
+++ text_transmission_decode.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 def file_read(file_path): #reads the file of inputs and returns the values as a list of amplitudes
     content = []
     with open(file_path, 'r') as file:
@@ -9,22 +10,35 @@ def file_read(file_path): #reads the file of inputs and returns the values as a 
     #print(content)
     return content
 def downconversion(amp_list, carrier_frequency, sampling_frequency):
-    sinlist = [0]*len(amp_list)
-    coslist = [0]*len(amp_list)
+    sinlist = np.zeros(len(amp_list))
+    coslist = np.zeros(len(amp_list))
     for i in range(len(sinlist)):
         sinlist[i] = amp_list[i]*math.sin(2*math.pi*carrier_frequency*i*(1/sampling_frequency))
     for i in range(len(sinlist)):
         coslist[i] = amp_list[i]*math.cos(2*math.pi*carrier_frequency*i*(1/sampling_frequency))
-    sinlist, coslist
-
-                   
     return sinlist, coslist
+def calc_fft(mat_size):
+    fft_mat = np.zeros((30, 30), dtype=np.complex128)
+    for row in range(mat_size):
+        for col in range(mat_size):
+            fft_mat[row][col] = math.e**complex(0, math.pi*2*(row*col)/mat_size)
+    return fft_mat
+def filter(downconverted_list):
+    for i in range(len(downconverted_list)):
+        if downconverted_list[i] < -5.1 or downconverted_list[i] > 5.1:
+            #print(downconverted_list[i])
+            downconverted_list[i] = 0
+    return downconverted_list
 filepath = "./input.txt"
 carrier_freq = 20 #carrier frequency in hertz
 sampling_frequency = 100 #sampling frequency in hertz
 
 amp_list = file_read(filepath) #get the list of amplitudes from the file
 sinlist, coslist = downconversion(amp_list, carrier_freq, sampling_frequency) #gets the downconverted sin and cos lists (needed for demodulation)
+filtered_sinlist = filter(sinlist)
+
+print(filtered_sinlist)
+print(calc_fft(30)[1][1])
 
 
 
